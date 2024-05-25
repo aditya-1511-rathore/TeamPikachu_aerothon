452ddbfa66966bfcbcda910fe677874ce8b52bfc
Aditya Singh <aditya.l.ls65@gmail.com>
2024-05-25 17:13:30 +0530

    Fixes

--- src/utils/get_weather.js
+++ src/utils/get_weather.js
@@ -3,7 +3,7 @@
 // import get_coordinates from './get_coordinates.js'
 
 // async function getWeatherDetailsForCoordinates(coordinates) {
-//     const apiKey = 'bc3b2504d252989228b1d03414fb01d3'; 
+//
 //     const weatherDetails = [];
 //     for (const coordinate of coordinates) {
 //         const { lat, lon } = coordinate;
@@ -45,52 +45,57 @@
 
 // export default get_weather
 
-import axios from 'axios';
-import get_coordinates from './get_coordinates.js';
+import axios from "axios";
+import get_coordinates from "./get_coordinates.js";
 
 async function getWeatherDetailsForCoordinates(coordinates) {
-    const apiKey = 'bc3b2504d252989228b1d03414fb01d3';
-    const weatherDetails = [];
-    for (const coordinate of coordinates) {
-        const { lat, lon } = coordinate;
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const apiKey = "API_KEY";
+  const weatherDetails = [];
+  for (const coordinate of coordinates) {
+    const { lat, lon } = coordinate;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
-        try {
-            const response = await axios.get(url);
-            const body = response.data;
-            var rain_volume = 0;
-            if (body.rain) {
-                rain_volume = body.rain['1h'];
-            }
-            const weatherInfo = {
-                latitude: body.coord.lat,
-                longitude: body.coord.lon,
-                temperature: body.main.temp,
-                visibility: body.visibility,
-                wind_speed: body.wind.speed,
-                rain_volume: rain_volume
-            };
+    try {
+      const response = await axios.get(url);
+      const body = response.data;
+      var rain_volume = 0;
+      if (body.rain) {
+        rain_volume = body.rain["1h"];
+      }
+      const weatherInfo = {
+        latitude: body.coord.lat,
+        longitude: body.coord.lon,
+        temperature: body.main.temp,
+        visibility: body.visibility,
+        wind_speed: body.wind.speed,
+        rain_volume: rain_volume,
+      };
 
-            weatherDetails.push(weatherInfo);
-        } catch (error) {
-            console.error(`Failed to fetch weather data for coordinates (${lat}, ${lon}):`, error.message);
-        }
+      weatherDetails.push(weatherInfo);
+    } catch (error) {
+      console.error(
+        `Failed to fetch weather data for coordinates (${lat}, ${lon}):`,
+        error.message
+      );
     }
+  }
 
-    return weatherDetails;
+  return weatherDetails;
 }
 
 const get_weather = async function (departureCity, arrivalCity) {
-    const coordinatesData = await get_coordinates(departureCity, arrivalCity);
-    const weather_details = await getWeatherDetailsForCoordinates(coordinatesData.points);
-    return {
-        weather_details,
-        coordinates: {
-            points: coordinatesData.points,
-            departure: coordinatesData.departure,
-            arrival: coordinatesData.arrival
-        }
-    };
-}
+  const coordinatesData = await get_coordinates(departureCity, arrivalCity);
+  const weather_details = await getWeatherDetailsForCoordinates(
+    coordinatesData.points
+  );
+  return {
+    weather_details,
+    coordinates: {
+      points: coordinatesData.points,
+      departure: coordinatesData.departure,
+      arrival: coordinatesData.arrival,
+    },
+  };
+};
 
 export default get_weather;
